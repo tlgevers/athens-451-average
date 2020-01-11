0d8cd0dcd291a3724d0315b5a733f8e581d2db2b
Trevor Gevers <trevor.gevers@ditoweb.com>
2020-01-11 17:34:28 -0600

    Calculate average and write output to file

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
     "io/ioutil"
     "strings"
     "strconv"
+    "os"
 )
 
 func readData() (data []byte, err error) {
@@ -19,26 +20,44 @@ func readData() (data []byte, err error) {
 func createArgs(d []byte) (data []string, err error) {
     args := strings.Fields(string(d))
     data = args
-    fmt.Println(len(args))
     return
 }
 
-func convInt(a []string) (v []int32, err error) {
-    var data []int32
+func convInt(a []string) (v []int, err error) {
+    var data []int
     for _, num := range a {
         d, _ := strconv.Atoi(num)
-        data = append(data, int32(d))
+        data = append(data, int(d))
         v = data
     }
     return
 }
 
-func sumArgs(args []int32) (sum int32, err error) {
+func sumArgs(args []int) (sum int, err error) {
     sum = 0
     for _, num := range args {
         sum += num
     }
     return
+}
+
+func average(args []int) (avg float32, err error) {
+    sum, err := sumArgs(args) 
+    if err != nil {
+        panic(err)
+    }
+    val := float32(sum)
+    avg = (val / float32(len(args)))
+    return
+}
+
+func writeData(avg string) {
+    f, err := os.Create("./output.txt")
+    if err != nil {
+        panic(err)
+    }
+    defer f.Close()
+    f.WriteString("The average is: " + avg)
 }
 
 func main() {
@@ -55,9 +74,10 @@ func main() {
     if err != nil {
         panic(err)
     }
-    sum, err := sumArgs(args)
+    avg, err := average(args)
     if err != nil {
         panic(err)
     }
-    fmt.Printf("The sum is: %d", sum)
+    roundedAvg := fmt.Sprintf("%.2f", avg)
+    writeData(roundedAvg)
 }
